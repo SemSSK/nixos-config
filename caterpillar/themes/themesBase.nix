45ee9a3545851a22506ead0ceeffff0bd811139b
semssk <ssksem015@gmail.com>
2026-02-08 17:38:44 +0100

    changed colorscheme to gruvbox

--- caterpillar/themes/themesBase.nix
+++ caterpillar/themes/themesBase.nix
@@ -5,7 +5,7 @@ in
 {
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
     image = wallpaperPath;
     polarity = "dark";
     cursor = {
